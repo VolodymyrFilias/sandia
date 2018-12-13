20453acd48a50f6fb83c9035fd1e9df4f9b60706
FiliasVolodymyr <vavan991143@gmail.com>
2018-12-13 14:45:31 +0200

    after translate

---
+++ js/contact-form.js
@@ -0,0 +1,88 @@
+$(document).ready(function () {
+    $("#applicationButton").bind("click", function () {
+        const xhttp = new XMLHttpRequest();
+        const botToken = "742595177:AAHiYKdIWD4mTo45ok--mpOLsGc1wZ3KIWI";
+        const chatId = "436669711";
+
+        let messageText = "";
+        let phoneNumber = $("#uPhoneInput").val();
+        let mailAdress = $("#uMailInput").val();
+        let projectDescription = $("#uName").val();
+
+        if (phoneNumber != "" && mailAdress != "" && projectDescription != "") {
+            messageText += "Ім'я: " +  projectDescription  +
+                "%0AНомер телефону: " + phoneNumber  +
+                "%0AПошта: " + mailAdress;
+            xhttp.open(`POST`, `https://api.telegram.org/bot${botToken}/sendMessage?chat_id=${chatId}&parse_mode=html&text=${messageText}`, false);
+            xhttp.send();
+        }
+    });
+});
+
+
+var container = document.getElementById('changeText');
+
+var things = ['бренды...',' веб-сайты...', 'упаковку...', 'чат-ботов...','рекламные ролики...'];
+var t = -1;
+var thing = '';
+var message = container.innerHTML;
+var mode = 'write';
+var delay = 1000;
+
+function updateText(txt) {
+    container.innerHTML = txt;
+}
+
+function tick() {
+    
+     if(container.innerHTML.length == 0) {
+        t++;
+        
+        thing = things[t];
+        message = '';
+        mode = 'write';
+        if (t == (things.length - 1)){
+            t= -1;
+        }
+        
+    }
+
+    switch(mode) {
+        case 'write' :
+            message += thing.slice(0, 1);
+            thing = thing.substr(1);
+
+            updateText(message);
+
+            if(thing.length === 0 && t === (things.length - 1)) {
+                window.clearTimeout(timeout);
+                return;
+            }
+
+            if(thing.length == 0){
+                mode = 'delete';
+                delay = 1500;
+            } else {
+                delay = 32 + Math.round(Math.random() * 40);
+            }
+
+            break;
+
+        case 'delete' :
+            message = message.slice(0, -1);
+            updateText(message);
+
+            if(message.length == 0)
+            {
+                mode = 'write';
+                delay = 1500;
+            } else {
+                delay = 32 + Math.round(Math.random() * 100);
+            }
+            break;
+    }
+
+    timeout = window.setTimeout(tick, delay);
+}
+
+var timeout = window.setTimeout(tick, delay);
